d0bbf15509ea042d8327dbc78aa15b9053ef91de
Sebastian Waisbrot <seppo0010@gmail.com>
2015-02-27 15:50:08 -0800

    Add travis tests

--- test/rlite.py
+++ test/rlite.py
@@ -10,6 +10,12 @@ class RliteTest(TestCase):
     def setUp(self):
         self.rlite = hirlite.Rlite()
 
+    def test_encoding(self):
+        snowman = b'\xe2\x98\x83'
+        self.assertEquals(snowman, self.rlite.command('ping', snowman))
+        self.rlite = hirlite.Rlite(encoding='utf8')
+        self.assertEquals(snowman.decode('utf-8'), self.rlite.command('ping', snowman))
+
     def test_none(self):
         self.assertEquals(None, self.rlite.command('get', 'hello'))
 
@@ -18,17 +24,21 @@ class RliteTest(TestCase):
 
     def test_string(self):
         self.rlite.command('set', 'hello', 'world')
-        self.assertEquals('world', self.rlite.command('get', 'hello'))
+        self.assertEquals(b'world', self.rlite.command('get', 'hello'))
 
     def test_integer(self):
         self.assertEquals(2, self.rlite.command('lpush', 'list', 'value', 'other value'))
 
     def test_error(self):
-        self.assertIsInstance(self.rlite.command('set', 'key'), hirlite.HirliteError)
+        result = self.rlite.command('set', 'key')
+        if hasattr(self, 'assertIsInstance'):
+            self.assertIsInstance(result, hirlite.HirliteError)
+        else:
+            self.assertTrue(isinstance(result, hirlite.HirliteError))
 
     def test_array(self):
         self.rlite.command('rpush', 'mylist', '1', '2', '3')
-        self.assertEquals(self.rlite.command('lrange', 'mylist', 0, -1), ['1', '2', '3'])
+        self.assertEquals(self.rlite.command('lrange', 'mylist', '0', '-1'), [b'1', b'2', b'3'])
 
 
 class PersistentTest(TestCase):
@@ -45,4 +55,4 @@ class PersistentTest(TestCase):
     def test_write_close_open(self):
         self.rlite.command('set', 'key', 'value')
         self.rlite = hirlite.Rlite(PersistentTest.PATH)  # close db, open a new one
-        self.assertEquals('value', self.rlite.command('get', 'key'))
+        self.assertEquals(b'value', self.rlite.command('get', 'key'))
