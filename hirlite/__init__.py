bfaf9d326fc0a2fc72a6f7b6ed92640c3fe9b87b
Dominik Miedziński <dominik@mdzn.pl>
2016-04-10 23:26:49 +0200

    Add support for calling commands by attr access

--- hirlite/__init__.py
+++ hirlite/__init__.py
@@ -1,4 +1,11 @@
-from .hirlite import Rlite, HirliteError
-from .version import __version__
+import functools
+
+from hirlite.hirlite import Rlite as RliteExtension, HirliteError
+from hirlite.version import __version__
 
 __all__ = ["Rlite", "HirliteError", "__version__"]
+
+
+class Rlite(RliteExtension):
+    def __getattr__(self, command):
+        return functools.partial(self.command, command)
